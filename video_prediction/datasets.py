7f3ff0ab65851a12e39659c9f3b5627be3df3a1a
Alex Lee <alexleegk@gmail.com>
2018-01-11 14:04:56 -0800

    Refactor video datasets to use hparams, analogous to models.

--- video_prediction/datasets.py
+++ video_prediction/datasets.py
@@ -1,16 +1,18 @@
 import glob
+import itertools
 import os
 import re
 from collections import OrderedDict
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.contrib.training import HParams
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.data.util import nest
 from tensorflow.python.framework import tensor_shape
 
 
-class FixedBatchDataset(dataset_ops.BatchDataset):
+class _FixedBatchDataset(dataset_ops.BatchDataset):
     @property
     def output_shapes(self):
         input_shapes = self._input_dataset.output_shapes
@@ -22,26 +24,18 @@ class FixedBatchDataset(dataset_ops.BatchDataset):
 
 class VideoDataset:
     def __init__(self, input_dir, mode='train', num_epochs=None,
-                 crop_size=None, scale_size=None,
-                 context_frames=None, sequence_length=None,
-                 frame_skip=None, time_shift=None, use_state=None):
+                 hparams_dict=None, hparams=None):
         """
         Args:
             input_dir: either a directory containing subdirectories train,
                 val, test, etc, or a directory containing the tfrecords.
             mode: either train, val, or test
             num_epochs: if None, dataset is iterated indefinitely.
-            crop_size: crop image into a square with sides of this length.
-            scale_size: resize image to this size after it has been cropped.
-            sequence_length: the number of frames in the video sequence, so
-                state-like sequences are of length sequence_length and
-                action-like sequences are of length sequence_length - 1.
-            frame_skip: number of frames to skip in between outputted frames,
-                so frame_skip=0 denotes no skipping.
-            time_shift: shift in time by multiples of this, so time_shift=1
-                denotes all possible shifts. time_shift=0 denotes no shifting.
-                It is ignored (equiv. to time_shift=0) when mode != 'train'.
-            use_state: whether to load and return state and actions.
+            hparams_dict: a dict of `name=value` pairs, where `name` must be
+                defined in `self.get_default_hparams()`.
+            hparams: a string of comma separated list of `name=value` pairs,
+                where `name` must be defined in `self.get_default_hparams()`.
+                These values overrides any values in hparams_dict (if any).
 
         Note:
             self.input_dir is the directory containing the tfrecords.
@@ -49,13 +43,6 @@ class VideoDataset:
         self.input_dir = os.path.normpath(os.path.expanduser(input_dir))
         self.mode = mode
         self.num_epochs = num_epochs
-        self.crop_size = crop_size
-        self.scale_size = scale_size
-        self.context_frames = 1 if context_frames is None else context_frames
-        self.sequence_length = sequence_length
-        self.frame_skip = 0 if frame_skip is None else frame_skip
-        self.time_shift = 1 if time_shift is None else time_shift
-        self.use_state = True if use_state is None else use_state
 
         if self.mode not in ('train', 'val', 'test'):
             raise ValueError('Invalid mode %s' % self.mode)
@@ -72,11 +59,48 @@ class VideoDataset:
                 break
         if not self.filenames:
             raise FileNotFoundError('No tfrecords were found in %s.' % self.input_dir)
+        self.dataset_name = os.path.basename(os.path.split(self.input_dir)[0])
 
         self.state_like_names_and_shapes = OrderedDict()
         self.action_like_names_and_shapes = OrderedDict()
         self._max_sequence_length = None
         self._dict_message = None
+
+        self.hparams = self.get_default_hparams().set_from_map(hparams_dict or {}).parse(hparams or '')
+
+    def get_default_hparams_dict(self):
+        """
+        Returns:
+            A dict with the following hyperparameters.
+
+            crop_size: crop image into a square with sides of this length.
+            scale_size: resize image to this size after it has been cropped.
+            context_frames: the number of ground-truth frames to pass in at
+                start.
+            sequence_length: the number of frames in the video sequence, so
+                state-like sequences are of length sequence_length and
+                action-like sequences are of length sequence_length - 1.
+                This number includes the context frames.
+            frame_skip: number of frames to skip in between outputted frames,
+                so frame_skip=0 denotes no skipping.
+            time_shift: shift in time by multiples of this, so time_shift=1
+                denotes all possible shifts. time_shift=0 denotes no shifting.
+                It is ignored (equiv. to time_shift=0) when mode != 'train'.
+            use_state: whether to load and return state and actions.
+        """
+        hparams = dict(
+            crop_size=None,
+            scale_size=None,
+            context_frames=1,
+            sequence_length=None,
+            frame_skip=0,
+            time_shift=1,
+            use_state=True,
+        )
+        return hparams
+
+    def get_default_hparams(self):
+        return HParams(**self.get_default_hparams_dict())
 
     @property
     def jpeg_encoding(self):
@@ -182,34 +206,37 @@ class VideoDataset:
                 action_like_seqs[example_name].append(features[name % i])
 
         # set sequence_length to the longest possible if it is not specified
-        if self.sequence_length is None:
-            self.sequence_length = (self._max_sequence_length - 1) // (self.frame_skip + 1) + 1
+        if self.hparams.sequence_length is None:
+            self.hparams.sequence_length = (self._max_sequence_length - 1) // (self.hparams.frame_skip + 1) + 1
 
         # handle random shifting and frame skip
-        if self.time_shift and self.mode == 'train':
-            assert self.time_shift > 0 and isinstance(self.time_shift, int)
-            num_shifts = ((self._max_sequence_length - 1) - (self.sequence_length - 1) * (self.frame_skip + 1)) // self.time_shift
+        sequence_length = self.hparams.sequence_length
+        frame_skip = self.hparams.frame_skip
+        time_shift = self.hparams.time_shift
+        if time_shift and self.mode == 'train':
+            assert time_shift > 0 and isinstance(time_shift, int)
+            num_shifts = ((self._max_sequence_length - 1) - (sequence_length - 1) * (frame_skip + 1)) // time_shift
             if num_shifts < 0:
                 raise ValueError('max_sequence_length has to be at least %d when '
                                  'sequence_length=%d, frame_skip=%d, but '
                                  'instead it is %d' %
-                                 ((self.sequence_length - 1) * (self.frame_skip + 1) + 1,
-                                  self.sequence_length, self.frame_skip, self._max_sequence_length))
-            t_start = tf.random_uniform([], 0, num_shifts + 1, dtype=tf.int32) * self.time_shift
+                                 ((sequence_length - 1) * (frame_skip + 1) + 1,
+                                  sequence_length, frame_skip, self._max_sequence_length))
+            t_start = tf.random_uniform([], 0, num_shifts + 1, dtype=tf.int32) * time_shift
         else:
             t_start = 0
-        state_like_t_slice = slice(t_start, t_start + (self.sequence_length - 1) * (self.frame_skip + 1) + 1, self.frame_skip + 1)
-        action_like_t_slice = slice(t_start, t_start + (self.sequence_length - 1) * (self.frame_skip + 1))
+        state_like_t_slice = slice(t_start, t_start + (sequence_length - 1) * (frame_skip + 1) + 1, frame_skip + 1)
+        action_like_t_slice = slice(t_start, t_start + (sequence_length - 1) * (frame_skip + 1))
 
         for example_name, seq in state_like_seqs.items():
             seq = tf.stack(seq)[state_like_t_slice]
-            seq.set_shape([self.sequence_length] + seq.shape.as_list()[1:])
+            seq.set_shape([sequence_length] + seq.shape.as_list()[1:])
             state_like_seqs[example_name] = seq
         for example_name, seq in action_like_seqs.items():
             seq = tf.stack(seq)[action_like_t_slice]
-            seq.set_shape([(self.sequence_length - 1) * (self.frame_skip + 1)] + seq.shape.as_list()[1:])
+            seq.set_shape([(sequence_length - 1) * (frame_skip + 1)] + seq.shape.as_list()[1:])
             # concatenate actions of skipped frames into single macro actions
-            seq = tf.reshape(seq, [self.sequence_length - 1, -1])
+            seq = tf.reshape(seq, [sequence_length - 1, -1])
             action_like_seqs[example_name] = seq
 
         return state_like_seqs, action_like_seqs
@@ -231,19 +258,19 @@ class VideoDataset:
             # shuffling.
             dataset = dataset.shuffle(buffer_size=min_queue_examples + 3 * batch_size)
 
-        dataset = FixedBatchDataset(dataset, batch_size)
+        dataset = _FixedBatchDataset(dataset, batch_size)
         iterator = dataset.make_one_shot_iterator()
         state_like_batches, action_like_batches = iterator.get_next()
 
         input_batches = OrderedDict(list(state_like_batches.items()) + list(action_like_batches.items()))
-        target_batches = state_like_batches['images'][:, self.context_frames:]
+        target_batches = state_like_batches['images'][:, self.hparams.context_frames:]
         return input_batches, target_batches
 
     def preprocess_image(self, image):
         """Preprocess a single image in [height, width, depth] layout."""
         image_shape = image.get_shape().as_list()
-        crop_size = self.crop_size
-        scale_size = self.scale_size
+        crop_size = self.hparams.crop_size
+        scale_size = self.hparams.scale_size
         if not crop_size:
             crop_size = min(image_shape[0], image_shape[1])
         image = tf.image.resize_image_with_crop_or_pad(image, crop_size, crop_size)
@@ -280,13 +307,18 @@ class GoogleRobotVideoDataset(VideoDataset):
     https://sites.google.com/site/brainrobotdata/home/push-dataset
     """
     def __init__(self, *args, **kwargs):
-        if kwargs.get('context_frames') is None:
-            kwargs['context_frames'] = 2
         super(GoogleRobotVideoDataset, self).__init__(*args, **kwargs)
         self.state_like_names_and_shapes['images'] = 'move/%d/image/encoded', (512, 640, 3)
-        if self.use_state:
+        if self.hparams.use_state:
             self.state_like_names_and_shapes['states'] = 'move/%d/endeffector/vec_pitch_yaw', (5,)
             self.action_like_names_and_shapes['actions'] = 'move/%d/commanded_pose/vec_pitch_yaw', (5,)
+
+    def get_default_hparams_dict(self):
+        default_hparams = super(GoogleRobotVideoDataset, self).get_default_hparams_dict()
+        hparams = dict(
+            context_frames=2,
+        )
+        return dict(itertools.chain(default_hparams.items(), hparams.items()))
 
     def num_examples_per_epoch(self):
         if os.path.basename(self.input_dir) == 'push_train':
@@ -310,18 +342,27 @@ class SV2PVideoDataset(VideoDataset):
         self.dataset_name = os.path.basename(os.path.split(self.input_dir)[0])
         self.state_like_names_and_shapes['images'] = 'image_%d', (64, 64, 3)
         if self.dataset_name == 'shape':
-            if self.use_state:
+            if self.hparams.use_state:
                 self.state_like_names_and_shapes['states'] = 'state_%d', (2,)
                 self.action_like_names_and_shapes['actions'] = 'action_%d', (2,)
         elif self.dataset_name == 'humans':
-            if kwargs.get('context_frames') is None:
-                kwargs['context_frames'] = 10
-            if kwargs.get('use_state') is None:  # default should be False only for humans dataset
-                self.use_state = False
-            if self.use_state:
+            if self.hparams.use_state:
                 raise ValueError('SV2PVideoDataset does not have states, use_state should be False')
         else:
             raise NotImplementedError
+
+    def get_default_hparams_dict(self):
+        default_hparams = super(SV2PVideoDataset, self).get_default_hparams_dict()
+        if self.dataset_name == 'shape':
+            hparams = dict()
+        elif self.dataset_name == 'humans':
+            hparams = dict(
+                context_frames=10,
+                use_state=False,
+            )
+        else:
+            raise NotImplementedError
+        return dict(itertools.chain(default_hparams.items(), hparams.items()))
 
     def num_examples_per_epoch(self):
         if self.dataset_name == 'shape':
@@ -354,17 +395,21 @@ class SoftmotionVideoDataset(VideoDataset):
     https://sites.google.com/view/sna-visual-mpc
     """
     def __init__(self, *args, **kwargs):
-        if kwargs.get('context_frames') is None:
-            kwargs['context_frames'] = 2
-        if kwargs.get('time_shift') is None:
-            kwargs['time_shift'] = 2  # for this dataset, default should be 2 instead of 1
         super(SoftmotionVideoDataset, self).__init__(*args, **kwargs)
         self.state_like_names_and_shapes['images'] = '%d/image_view0/encoded', (64, 64, 3)
-        if self.use_state:
+        if self.hparams.use_state:
             self.state_like_names_and_shapes['states'] = '%d/endeffector_pos', (3,)
             self.action_like_names_and_shapes['actions'] = '%d/action', (4,)
         if os.path.basename(self.input_dir).endswith('annotation'):
             self.state_like_names_and_shapes['object_pos'] = '%d/object_pos', (2,)
+
+    def get_default_hparams_dict(self):
+        default_hparams = super(SoftmotionVideoDataset, self).get_default_hparams_dict()
+        hparams = dict(
+            context_frames=2,
+            time_shift=2,
+        )
+        return dict(itertools.chain(default_hparams.items(), hparams.items()))
 
     @property
     def jpeg_encoding(self):
