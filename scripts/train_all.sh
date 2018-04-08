541223fc816d52e2187d52be6cc06525ac41f02e
Alex Lee <alexleegk@gmail.com>
2018-04-07 20:19:17 -0700

    Add train_all.sh script (untested).

---
+++ scripts/train_all.sh
@@ -0,0 +1,40 @@
+# BAIR action-free robot pushing dataset
+for model in \
+  ours_deterministic_l1 \
+  ours_deterministic_l2 \
+  ours_vae_l1 \
+  ours_vae_l2 \
+  ours_gan \
+  ours_savp \
+; do
+  CUDA_VISIBLE_DEVICES=0 python scripts/train.py --input_dir data/bair --dataset bair --model savp --model_hparams_dict hparams/bair_action_free/${model}.json --logs_dir logs/bair_action_free
+done
+
+# KTH human actions dataset
+for model in \
+  ours_deterministic_l1 \
+  ours_deterministic_l2 \
+  ours_vae_l1 \
+  ours_gan \
+  ours_savp \
+; do
+  CUDA_VISIBLE_DEVICES=0 python scripts/train.py --input_dir data/kth--dataset kth --model savp --model_hparams_dict hparams/kth/${model}.json --logs_dir logs/kth
+done
+
+# BAIR action-conditioned robot pushing dataset
+for model in \
+  ours_deterministic_l1 \
+  ours_deterministic_l2 \
+  ours_vae_l1 \
+  ours_vae_l2 \
+  ours_gan \
+  ours_savp \
+; do
+  CUDA_VISIBLE_DEVICES=0 python scripts/train.py --input_dir data/bair --dataset bair --dataset_hparams use_state=True --model savp --model_hparams_dict hparams/bair/${model}.json --logs_dir logs/bair
+done
+for model in \
+  sna_l1 \
+  sna_l2 \
+; do
+  CUDA_VISIBLE_DEVICES=0 python scripts/train.py --input_dir data/bair --dataset bair --dataset_hparams use_state=True --model sna --model_hparams_dict hparams/bair/${model}.json --logs_dir logs/bair
+done
